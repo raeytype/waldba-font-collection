b85ba088cf3ee4894c9f793b0d68a654dcbf842f
dyacob <yacob@geez.org>
2025-07-11 14:52:24 -0400

    Adjusted for .glyphs files.

--- source/makefonts.sh
+++ source/makefonts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for font in  *.ufo ; do
-  fontmake -u $font --overlaps-backend pathops -o ttf
+# for font in  *.ufo ; do
+  # fontmake -u $font --overlaps-backend pathops -o ttf
+for font in  *.glyphs ; do
+  fontmake -g $font --overlaps-backend pathops -o ttf
 done
