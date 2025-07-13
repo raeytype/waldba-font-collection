8b4937918d978f25fc04f31d26e34f2ecf905a03
dyacob <yacob@geez.org>
2025-07-13 14:46:33 -0400

    Adding script to remove Mac Roman duplicate names.

--- source/WORKFLOW.md
+++ source/WORKFLOW.md
@@ -5,11 +5,15 @@ yet support (easily) color output to OpenType fonts.  So do this:
 
 
 1. Export font form FontLab Studio to "OpenType COLR"
-2. Execute in Python 3: `add-cmap-from-file.py`
+2. Execute in Python 3: `waldba-fix-fontnames.py`
+3. Execute in Python 3: `add-cmap-from-file.py`
 
 For example:
 ```
+waldba-fix-fontnames.py  &lt;TTF in file&gt; &lt;TTF out file&gt;
 add-cmap-from-file.py &lt;TTF in file&gt; &lt;TTF out file&gt; &lt;UVS mapping file&gt;
-add-cmap-from-file.py /path/to/WaldbaFantuwua-Regular-COLR.ttf  WaldbaFantuwua-COLR-UVS.ttf Waldba.uvs
+
+waldba-fix-fontnames.py /path/to/WaldbaFantuwua-Regular-COLR.ttf  /tmp/WaldbaFantuwua-NameFix.ttf Waldba.uvs
+add-cmap-from-file.py /tmp/WaldbaFantuwua-NameFix.ttf  WaldbaFantuwua-COLR-UVS.ttf Waldba.uvs
 ```
 The `.uvs` file follows the format used by HighLogic's FontCreator tool.
---
+++ source/waldba-fix-fontnames.py
@@ -0,0 +1,36 @@
+#
+# This script remvoes the duplicate name entries in Mac Roman encoding,
+# which appear to override the Unicode encoded names on OSX
+#
+import sys
+from fontTools import ttLib
+
+
+if __name__ == "__main__":
+        font_path = sys.argv[2]
+
+        font = ttLib.TTFont(font_path)
+	# Name ID, Platform ID, Platform Encoding ID, Language ID
+	# Platform ID = (Apple)
+	# Platform Encoding ID = 0 (Mac Roman)
+	# Language ID = 0 (English?)
+	# 
+        # Name ID: 
+	#   1 = Font Family Name
+	#   2 = Font Subfamily Name
+	#   4 = Full Name
+	#   5 = Version string
+	#   6 = PostScript name
+
+        font["name"].removeNames(1, 1, 0, 0)
+        font["name"].removeNames(2, 1, 0, 0)
+        font["name"].removeNames(4, 1, 0, 0)
+        font["name"].removeNames(5, 1, 0, 0)
+        font["name"].removeNames(6, 1, 0, 0)
+        namerecord_list = font["name"].names
+
+        # determine font style for this file path from name record nameID 2
+        for record in namerecord_list:
+            print( record.nameID , ": ", record.string )
+
+        font.save( sys.argv[3] )
